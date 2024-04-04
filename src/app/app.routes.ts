26b24da1af3367ec770aabba98b7b8ae3bc65b8f
Pratik Vairat <pratikvairat30@gmail.com>
2024-04-04 12:30:40 +0530

    SPPU assignment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'register', component: RegisterComponent },
 ];
